1e69d067e1a0b5d367b81f29f795b342cc15627c
Manuela Lorenzi-Kayser <manuela.lkayser@gmail.com>
2020-03-11 15:13:30 -0400

    Simplified list of constants.

--- constants.js
+++ constants.js
@@ -1,27 +1,7 @@
 module.exports = {
     WORKSHEET_NAME: 'DMA',
     SPEND_CATEGORIES: [
-        'Television',
-        'Streaming Television',
-        'Radio',
-        'Streaming Radio',
-        'Direct Mail',
-        'Shared Mail',
-        'Letter',
-        'Email',
-        'Digital Display',
-        'SEO/SEM',
-        'Social',
-        'OOH',
-        'Newspaper',
-        'Events',
-        'Sponsorship(s)',
-        'Guerilla Marketing',
-        'Promotional POP',
-        'Public Relations Fee',
-        'Production Fees',
-        'Agency Fees',
-        'Reoccurring Co-Op Contribution'
+        'Total Expenses'
     ],
     TEMPLATE_VERSION: 2
 };
